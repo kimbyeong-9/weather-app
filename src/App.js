3968a75f7f807643a00b27352da508268bd7df8f
byeongho Kim <gkrjawnrkf1@gmail.com>
2025-04-23 01:09:51 +0900

    누나 2리액트프로젝트완료

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton';
+import { ClipLoader } from "react-spinners";
+
 function App() {
 
   const [weather, setWeather] = useState(null)
-  const cities=['Hanoi','Hochiminh','Seoul','New york','Tokyo']
+  const [city, setCity] = useState("")
+  const [loading, setLoading] = useState(false);
+  const cities=['Hanoi','Ho Chi Minh','Seoul','New york','Tokyo']
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position)=>{
       let lat = position.coords.latitude;
@@ -17,21 +21,43 @@ function App() {
 
   const getWeatherByCurrentLocation = async(lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=dcdc2c70ac8ad9a65b4859bbdda2c0b3&units=metric`;
+    setLoading(true);
     let response = await fetch(url)
     let data = await response.json();
     setWeather(data);
+    setLoading(false);
   };
 
+  const getWeatherByCity = async() => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=dcdc2c70ac8ad9a65b4859bbdda2c0b3&units=metric`;
+    setLoading(true);
+    let response = await fetch(url);
+    let data = await response.json();
+    setWeather(data);
+    setLoading(false);
+  }
+
   useEffect(() => {
-    getCurrentLocation()
-  },[]);
+    if (city == "") {
+      getCurrentLocation();
+    } else {
+      getWeatherByCity();
+    }   
+  },[city]);
+
 
   return (
    <div>
+    {loading? (
+      <div className="container">
+       <ClipLoader color = "#f88c6b" loading={loading} size={150} />
+      </div>
+    ):(
     <div className="container">
       <WeatherBox weather={weather} />
-      <WeatherButton cities={cities} />
+      <WeatherButton cities={cities} setCity={setCity}/>
     </div>
+    )}
   </div>
   );
 }
--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -6,7 +6,7 @@ const WeatherBox = ({weather}) => {
   return (
     <div className="weather-box">
       <div>{weather?.name}</div>
-      <h2>{weather?.main.temp}°C /{weather?.main.temp * 9 / 5 + 32}</h2>
+      <h2>{weather?.main.temp}°C /{weather?.main.temp * 9 / 5 + 32}화씨</h2>
       <h3>{weather?.weather[0].description}</h3>
     </div>
   )
